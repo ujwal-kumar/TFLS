c57700ac4ebee43919edb4dabc8a8d761352e314
ujwal-kumar <ujwal.prodevans@gmail.com>
2018-11-28 12:45:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,6 +41,12 @@ resource "aws_security_group" "allow_all" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "demo" {
