bae9581a290ef48b0fb5d69abf37a3bb274a56f3
ujwal-kumar <ujwal.prodevans@gmail.com>
2018-11-28 13:35:32 +0530

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo yum -y install epel-release phpmyadmin rpm-build redhat-rpm-config;
-sudo yum -y install mysql-community-release-el7-5.noarch.rpm;
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm;
+sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm;
 sudo yum -y install httpd mysql-server mysql-devel php php-mysql php-fpm;
-sudo yum -y install tinyxml libzip mysql-workbench-community;
+#sudo yum -y install tinyxml libzip mysql-workbench-community;
 sudo chmod 777 -R /var/www/;
 sudo printf "<?php\nphpinfo();\n?>" > /var/www/html/info.php;
 sudo service mysqld restart;
