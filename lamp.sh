baad8d4df3c2eab6ed9c6a9c95bbffbc85a0cabb
ujwal-kumar <ujwal.prodevans@gmail.com>
2018-11-30 16:14:02 +0530

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -6,7 +6,7 @@ sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm;
 sudo yum -y install httpd mysql-server mysql-devel php php-mysql php-fpm;
 #sudo yum -y install tinyxml libzip mysql-workbench-community;
 sudo chmod 777 -R /var/www/;
-sudo printf "<?php\nphpinfo();\n?>" > /var/www/html/info.php;
+sudo cp /tmp/index.php /var/www/html/;
 sudo service mysqld restart;
 sudo service httpd restart;
 sudo chkconfig httpd on;
